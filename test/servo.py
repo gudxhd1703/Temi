c3ab59be52c1718dfb91ebd2366c88cf43830590
KHJ <gudxhd1703@gmail.com>
2023-11-21 18:05:54 +0900

    servo

--- test/servo.py
+++ test/servo.py
@@ -14,35 +14,18 @@ servo1 = servo.Servo(pca.channels[1])
 servo2 = servo.Servo(pca.channels[2])
 servo3 = servo.Servo(pca.channels[3])
 servo4 = servo.Servo(pca.channels[4])
-<<<<<<< HEAD
-
 
 try:
     while True:
-        servo1.angle = 90
-        servo2.angle = 90
-        servo3.angle = 0
-        servo4.angle = 0
+        angle = int(input("angle:"))
+        servo1.angle = angle
+        servo2.angle = angle
+        servo3.angle = angle
+        servo4.angle = angle
+        time.sleep(1)
 except KeyboardInterrupt:
     print("프로그램 종료")
 finally:    
     pca.reset()
     pca.deinit()
-=======
-time.sleep(1)
-
-# We sleep in the loops to give the servo time to move into position.
-#try:
-#    while True:
-servo1.angle = 70
-servo2.angle = 70
-servo3.angle = 70
-servo4.angle = 70
-time.sleep(2)
-
-#except KeyboardInterrupt:
-pca.deinit()
->>>>>>> 442fc30 (test)
-   
-# You can also specify the movement fractionally.
 
